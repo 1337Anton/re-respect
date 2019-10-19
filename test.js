3858b268adb222ce0d427ccd92992d3bfc97edb0
Coco Rave <C:\Users\Coco\AppData\Roaming\The Bat!>
2019-10-19 15:13:52 +0200

    ok

---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+* <re:respect>
+* Jugend hackt
+* Autoren: Anton, Manolo, Björn, Felix 
+* TODO: 
+*/
+
+setInterval(function() {
+    var a = document.querySelectorAll("input[type=email]");
+    console.log(a); 
+}, 3000);
