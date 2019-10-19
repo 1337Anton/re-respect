481e808de406b1ebcb5d5826c0a65a6f8eec2bc1
Coco Rave <C:\Users\Coco\AppData\Roaming\The Bat!>
2019-10-19 15:54:45 +0200

    push

--- test.js
+++ test.js
@@ -5,7 +5,11 @@
 * TODO: 
 */
 
-setInterval(function() {
-    var a = document.querySelectorAll("input[type=email]");
-    console.log(a); 
-}, 3000);
+var email = "ok@gmail.com"; 
+
+window.a = document.querySelectorAll("input[type=email]");
+b = a[0];
+b.value = email;
+
+//setInterval(function() {
+//}, 3000);
