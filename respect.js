98f7b6fa4ac66a2bb3aa5e2d506d0fcedf001ca9
Coco Rave <C:\Users\Coco\AppData\Roaming\The Bat!>
2019-10-20 11:59:02 +0200

    k

--- respect.js
+++ respect.js
@@ -15,13 +15,13 @@ const url = sessionStorage.getItem("c24df9856f4a06331a6ae9d9393a79b6");
 // email field
 window.emailf = document.querySelectorAll("input[type=email]");
 emailf = emailf[0];
-emailf.value = "mail@gmail.com";
+emailf.value = email;
 
 // text fields
 window.textfs = document.querySelectorAll("input[type=text]");
 
 namef = textfs[0];
-namef.value = "Enter full name here.";
+namef.value = name;
 
 titlef = textfs[2];
 titlef.value = "Enter title here.";
