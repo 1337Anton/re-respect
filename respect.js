9ee3c774d4501a727297b8e53cd36872c8db4e1a
Coco Rave <C:\Users\Coco\AppData\Roaming\The Bat!>
2019-10-20 10:38:54 +0200

    ok

--- respect.js
+++ respect.js
@@ -5,6 +5,11 @@
 * TODO: 
 */
 
+function pageScroll() {
+    window.scrollBy(0,1);
+    scrolldelay = setTimeout(pageScroll,10);
+}
+
 // user data
 const email = sessionStorage.getItem("cb7b13818e85271c850a774ee3237f08");
 const url = sessionStorage.getItem("c24df9856f4a06331a6ae9d9393a79b6");
@@ -12,7 +17,7 @@ const url = sessionStorage.getItem("c24df9856f4a06331a6ae9d9393a79b6");
 // email field
 window.emailf = document.querySelectorAll("input[type=email]");
 emailf = emailf[0];
-emailf.value = email;
+emailf.value = "mail@gmail.com";
 
 // text fields
 window.textfs = document.querySelectorAll("input[type=text]");
@@ -24,4 +29,14 @@ titlef = textfs[2];
 titlef.value = "Enter title here.";
 
 urlf = textfs[3];
-urlf.value = url;
+urlf.value = "url";
+
+// text area
+window.textareaf = document.getElementsByClassName("redactor-editor redactor-linebreaks");
+textareaf = textareaf[0];
+console.log(textareaf);
+textareaf.click();
+textareaf.innerText = "Text here";
+
+pageScroll();
+
